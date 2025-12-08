0ccbc9a02de25c9f795feee178eb397ccb71f212
michaelrenkema <michael@renkema.nz>
2025-12-09 05:54:14 +1300

    Cater for array types as dependency member parameters

--- Sut.Generator/SutDependencyMemberParameter.cs
+++ Sut.Generator/SutDependencyMemberParameter.cs
@@ -1,8 +1,11 @@
+using Microsoft.CodeAnalysis;
+
 namespace Sut.Generator;
 
-public readonly struct SutDependencyMemberParameter(string name, string? typeName, string? typeNamespace, EquatableList<SutTypeArgument> typeArguments)
+public readonly struct SutDependencyMemberParameter(string name, TypeKind? typeKind, string? typeName, string? typeNamespace, EquatableList<SutTypeArgument> typeArguments)
 {
   public string Name { get; init; } = name;
+  public TypeKind? TypeKind { get; init; } = typeKind;
   public string? TypeName { get; init; } = typeName;
   public string? TypeNamespace { get; init; } = typeNamespace;
   public EquatableList<SutTypeArgument> TypeArguments { get; init; } = typeArguments;
@@ -10,6 +13,7 @@ public readonly struct SutDependencyMemberParameter(string name, string? typeNam
   public bool Equals(SutDependencyMemberParameter other)
   {
     return this.Name == other.Name &&
+      !(this.TypeKind is null || other.TypeKind is null || this.TypeKind != other.TypeKind) &&
       !(this.TypeName is null || other.TypeName is null || this.TypeName != other.TypeName) &&
       !(this.TypeNamespace is null || other.TypeNamespace is null || this.TypeNamespace != other.TypeNamespace) &&
       this.TypeArguments.Equals(other.TypeArguments);
